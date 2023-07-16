abd84eabd3cce7d0de180404409b1355b4ca8534
Valeri Sokolov <ulfurinn@ulfurinn.net>
2023-07-16 14:52:13 +0200

    remove root supervisor

--- mix.exs
+++ mix.exs
@@ -15,7 +15,6 @@ defmodule Wongi.Engine.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {Wongi.Engine.Application, []},
       extra_applications: [:logger]
     ]
   end
