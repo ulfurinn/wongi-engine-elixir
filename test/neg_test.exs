ed3370acfe2d7b57ceb100680c84310f6f75ca93
Valeri Sokolov <ulfurinn@ulfurinn.net>
2023-07-16 23:29:12 +0200

    test: neg with vars

--- test/neg_test.exs
+++ test/neg_test.exs
@@ -45,4 +45,30 @@ defmodule Wongi.NegTest do
     rete = rete |> Engine.retract([:x, :y, :z])
     assert [] = rete |> Engine.tokens(ref) |> MapSet.to_list()
   end
+
+  test "tests against variables" do
+    {ref, rete} =
+      Engine.new()
+      |> Engine.compile_and_get_ref(
+        rule(
+          forall: [
+            has(:a, :b, var(:x)),
+            neg(:x, :y, var(:x))
+          ]
+        )
+      )
+
+    rete =
+      rete
+      |> Engine.assert([:a, :b, 1])
+      |> Engine.assert([:x, :y, 1])
+
+    assert [] = rete |> Engine.tokens(ref) |> MapSet.to_list()
+
+    rete =
+      rete
+      |> Engine.retract([:x, :y, 1])
+
+    assert [_] = rete |> Engine.tokens(ref) |> MapSet.to_list()
+  end
 end
