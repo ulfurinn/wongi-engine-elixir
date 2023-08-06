a9bc62deef444bdba7b0b68c0d15157a1f731cae
Valeri Sokolov <ulfurinn@ulfurinn.net>
2023-08-06 14:51:43 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Wongi.Engine.MixProject do
   def project do
     [
       app: :wongi_engine,
-      version: "0.9.4",
+      version: "0.9.5",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
