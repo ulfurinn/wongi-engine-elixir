e383f2f591cf8c997b47f419c2b98e705b314235
Valeri Sokolov <ulfurinn@ulfurinn.net>
2023-08-20 12:34:05 +0200

    feat: ternary functions as actions

--- lib/wongi/engine/action.ex
+++ lib/wongi/engine/action.ex
@@ -14,7 +14,21 @@ defimpl Wongi.Engine.Action, for: Function do
     end
   end
 
-  def deexecute(_fun, _token, rete) do
+  def execute(fun, token, rete) when is_function(fun, 3) do
+    case fun.(:execute, token, rete) do
+      %Rete{} = updated -> updated
+      _ -> rete
+    end
+  end
+
+  def deexecute(fun, _, rete) when is_function(fun, 2) do
     rete
+  end
+
+  def deexecute(fun, token, rete) when is_function(fun, 3) do
+    case fun.(:deexecute, token, rete) do
+      %Rete{} = updated -> updated
+      _ -> rete
+    end
   end
 end
--- test/wongi/engine/action_test.exs
+++ test/wongi/engine/action_test.exs
@@ -1,7 +1,7 @@
 defmodule Wongi.Engine.ActionTest do
   use Wongi.TestCase
 
-  test "using a function as an action" do
+  test "using a binary function as an action" do
     _rete =
       new()
       |> compile(
@@ -15,7 +15,28 @@ defmodule Wongi.Engine.ActionTest do
         )
       )
       |> assert(:a, :b, 1)
+      |> retract(:a, :b, 1)
 
     assert_received {:action, 1}
+  end
+
+  test "using a ternary function as an action" do
+    _rete =
+      new()
+      |> compile(
+        rule(
+          forall: [
+            has(:a, :b, var(:x))
+          ],
+          do: [
+            fn action, token, _rete -> send(self(), {action, token[:x]}) end
+          ]
+        )
+      )
+      |> assert(:a, :b, 1)
+      |> retract(:a, :b, 1)
+
+    assert_received {:execute, 1}
+    assert_received {:deexecute, 1}
   end
 end
